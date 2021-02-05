8499ed8ad8524ff871fa7bf1b8f0b805b94a526a
Fafofafoy <ihcaoe@gmail.com>
2021-02-05 08:42:51 +0800

    處理目錄

--- crawler.py
+++ crawler.py
@@ -2,60 +2,39 @@
 from urllib.parse import urljoin
 import scrapy
 from scrapy.selector import Selector
-
+from sys import stderr
 
 class Spider(scrapy.Spider):
     name = "ami"
     allowed_domains = ["e-dictionary.apc.gov.tw"]
     download_delay = 0
-    辭典網址 = 'http://e-dictionary.apc.gov.tw/{}/Search.htm'
-    目錄網址 = 'http://e-dictionary.apc.gov.tw/{}/TermsMenu.htm'
-    詞條網址 = 'http://e-dictionary.apc.gov.tw/{}/Term.htm'
+    辭典網址 = 'https://e-dictionary.apc.gov.tw/{}/terms.htm'
 
     custom_settings = {
         'FEED_EXPORT_ENCODING': 'utf-8',
     }
 
     def __init__(self, lang='ami', ad=None, *args, **kwargs):
-        super(Spider, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
         self.start_urls = [self.辭典網址.format(lang)]
         self.lang = lang
 
     def parse(self, response):
-        for 目錄選項 in Selector(response).xpath(
-            '//select[@id="ctl00_oCPH_Tabs_ddl_char"]/option'
+        yield from self.掠詞條(response)
+        for a in response.css(
+            'div.wordlist_select ul.picker li a'
         ):
-            字首 = 目錄選項.xpath('text()').extract_first()
-            編號 = 目錄選項.xpath('@value').extract_first()
-            meta = {
-                #                 'cookiejar': "%s_%d" % (self.lang, i),
-                '字首': 字首,
-                '目錄編號': 編號,
+            yield {
+                'href': a.attrib['href'],
+                'text': a.css('a::text').get(),
             }
-            yield scrapy.FormRequest(
-                self.目錄網址.format(self.lang, 編號),
-                method='POST',
-                formdata={'tsid': 編號},
-                meta=meta, dont_filter=True,
-                callback=self.掠目錄,
-            )
-
-    def 掠目錄(self, response):
-        self.logger.debug('掠 {} 的目錄'.format(response.meta['字首']))
-        for 詞條選項 in Selector(response).xpath('//a[@class="w_term"]'):
-            詞條編號 = 詞條選項.xpath('@rel').extract_first()
-            詞條名 = 詞條選項.xpath('text()').extract_first()
-            meta = {
-                #                 'cookiejar': "%s_%d" % (self.lang, i),
-                '詞條名': 詞條名,
+        for a in response.css(
+            'div.wordlist_scrollcontent ul.list_box li a.w_term'
+        ):
+            yield {
+                'href': a.attrib['href'],
+                'text': a.css('a::text').get(),
             }
-            yield scrapy.FormRequest(
-                self.詞條網址.format(self.lang, 詞條編號),
-                method='POST',
-                formdata={'did': 詞條編號, 'fun': ''},
-                meta=meta, dont_filter=True,
-                callback=self.掠詞條,
-            )
 
     def 掠詞條(self, response):
         self.logger.debug('掠 {} 詞條'.format(response.meta['詞條名']))
