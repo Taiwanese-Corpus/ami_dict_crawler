e96d3396ff4351a199b9a5a08a0273ecf443349b
Fafofafoy <ihcaoe@gmail.com>
2021-02-09 21:38:59 +0800

    補bô詞性ê試驗

---
+++ test/test_sului.py
@@ -0,0 +1,17 @@
+from test import SuTiau
+
+
+class O(SuTiau):
+    url = (
+        'https://e-dictionary.apc.gov.tw'
+        '/trv/terms/m/115.htm'
+    )
+
+    def test_bohunlui(self):
+        self.assertEqual(self.sutiau['sutiau'], 'o', self.sutiau)
+
+    def test_késueh_susing(self):
+        self.assertEqual(
+            self.sutiau['kesueh'][0]['詞類'], '',
+            self.sutiau
+        )
